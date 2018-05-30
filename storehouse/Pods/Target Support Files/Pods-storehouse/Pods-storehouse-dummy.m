61b278aa80762f5f58cc7f3b73353e19d866d68c
jdcwy <414266852@qq.com>
2018-05-30 20:57:40 +0800

    新建项目

---
+++ storehouse/Pods/Target Support Files/Pods-storehouse/Pods-storehouse-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_storehouse : NSObject
+@end
+@implementation PodsDummy_Pods_storehouse
+@end
---
+++ storehouse/storehouse/Classes/Assets/AssetsVC.m
@@ -0,0 +1,38 @@
+//
+//  AssetsVC.m
+//  storehouse
+//
+//  Created by 万宇 on 2018/5/30.
+//  Copyright © 2018年 wanyu. All rights reserved.
+//
+
+#import "AssetsVC.h"
+
+@interface AssetsVC ()
+
+@end
+
+@implementation AssetsVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @"资产";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
