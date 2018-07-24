f90e43d82684a8469008b0b214532c3c6628c195
jdcwy <414266852@qq.com>
2018-07-24 16:41:43 +0800

    采购申请选项列表

--- storehouse/storehouse/Classes/Apply/C/LaunchPurchaseVC.m
+++ storehouse/storehouse/Classes/Apply/C/LaunchPurchaseVC.m
@@ -8,9 +8,11 @@
 
 #import "LaunchPurchaseVC.h"
 #import "LaunchBaseTVCell.h"
+#import "ZWPullMenuView.h"
 
 static NSString* tableCellid = @"table_cell";
-@interface LaunchPurchaseVC ()<UITableViewDelegate,UITableViewDataSource>
+@interface LaunchPurchaseVC ()<UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate>
+@property (nonatomic, strong) NSArray *itemTypeArray;
 
 @end
 
@@ -20,6 +22,7 @@ static NSString* tableCellid = @"table_cell";
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.title = @"采购申请";
+    self.itemTypeArray = [NSArray arrayWithObjects:@"申请部门",@"物品名称",@"规格型号",@"计量单位",@"预算价格",@"采购数量",@"生产厂家",@"采购类别",@"采购理由", nil];
     self.view.backgroundColor = [UIColor colorWithHexString:@"f1f1f1"];
     self.tableView.backgroundColor = [UIColor colorWithHexString:@"f1f1f1"];
     self.tableView.scrollEnabled = false;
@@ -34,16 +37,56 @@ static NSString* tableCellid = @"table_cell";
     
 }
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 9;//
+    return self.itemTypeArray.count;//
     
 }
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     LaunchBaseTVCell *cell = [tableView dequeueReusableCellWithIdentifier:tableCellid forIndexPath:indexPath];
     if (indexPath.row == 0 || indexPath.row == 3 || indexPath.row == 7) {
         cell.listButton.hidden = false;
+        cell.listButton.tag = 100+indexPath.row;
+        [cell.listButton addTarget:self action:@selector(listButtonClick:) forControlEvents:UIControlEventTouchUpInside];
     }
-    //    cell.model = self.recordArr[indexPath.row];
+    cell.itemLabel.text = self.itemTypeArray[indexPath.row];
+    cell.contentField.delegate = self;
     return cell;
+}
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:true];
+}
+//列表按钮点击事件
+-(void)listButtonClick:(UIButton*)sender{
+    LaunchBaseTVCell *curruntCell = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:(sender.tag-100) inSection:0]];
+    if (sender.tag == 100) {
+        ZWPullMenuView *menuView = [ZWPullMenuView pullMenuAnchorView:sender titleArray:@[@"软件部",@"财务部",@"行政部",@"人力资源部"]];
+        menuView.zwPullMenuStyle = PullMenuLightStyle;
+        __weak typeof(menuView) weakMenuView = menuView;
+        menuView.blockSelectedMenu = ^(NSInteger menuRow) {
+            NSLog(@"action----->%ld",(long)menuRow);//menuRow为点击的行号
+            curruntCell.contentField.text = weakMenuView.titleArray[menuRow];
+        };
+    }else if (sender.tag == 103){
+        ZWPullMenuView *menuView = [ZWPullMenuView pullMenuAnchorView:sender titleArray:@[@"台",@"个",@"只"]];
+        menuView.zwPullMenuStyle = PullMenuLightStyle;
+        __weak typeof(menuView) weakMenuView = menuView;
+        menuView.blockSelectedMenu = ^(NSInteger menuRow) {
+            NSLog(@"action----->%ld",(long)menuRow);//menuRow为点击的行号
+            curruntCell.contentField.text = weakMenuView.titleArray[menuRow];
+        };
+    }else{
+        ZWPullMenuView *menuView = [ZWPullMenuView pullMenuAnchorView:sender titleArray:@[@"计划内",@"计划外"]];
+        menuView.zwPullMenuStyle = PullMenuLightStyle;
+        __weak typeof(menuView) weakMenuView = menuView;
+        menuView.blockSelectedMenu = ^(NSInteger menuRow) {
+            NSLog(@"action----->%ld",(long)menuRow);//menuRow为点击的行号
+            curruntCell.contentField.text = weakMenuView.titleArray[menuRow];
+        };
+    }
+    
+}
+//设置列表行为不可编辑
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
+    return false;
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
--- storehouse/storehouse/Classes/Apply/V/LaunchBaseTVCell.m
+++ storehouse/storehouse/Classes/Apply/V/LaunchBaseTVCell.m
@@ -21,6 +21,7 @@
     return self;
 }
 - (void)setupUI{
+    self.selectionStyle = UITableViewCellSelectionStyleNone;//取消选中效果
     //事项label
     UILabel *itemLabel = [UILabel labelWithText:@"申请部门" andTextColor:[UIColor colorWithHexString:@"373a41"] andFontSize:12];
     [self.contentView addSubview:itemLabel];
@@ -29,8 +30,17 @@
         make.left.offset(15*kiphone6);
         make.width.offset(60*kiphone6);
     }];
+    self.itemLabel = itemLabel;
     //内容textfiled
     UITextField *conentField = [[UITextField alloc]init];
+    //设置左边视图的宽度
+    
+    conentField.leftView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 9, 0)];
+    conentField.font = [UIFont systemFontOfSize:12];
+    conentField.textColor = [UIColor colorWithHexString:@"373a41"];
+    //设置显示模式为永远显示(默认不显示 必须设置 否则没有效果)
+    
+    conentField.leftViewMode = UITextFieldViewModeAlways;
     conentField.backgroundColor = [UIColor whiteColor];
     conentField.layer.borderColor = [UIColor colorWithHexString:@"a0a0a0"].CGColor;
     conentField.layer.borderWidth = 1;
@@ -41,6 +51,8 @@
         make.right.offset(-31*kiphone6);
         make.height.offset(35*kiphone6H);
     }];
+    self.contentField = conentField;
+    //列表按钮
     UIButton *listButton = [[UIButton alloc]init];
     [listButton setImage:[UIImage imageNamed:@"list_swich"] forState:UIControlStateNormal];
     listButton.backgroundColor = [UIColor colorWithHexString:@"d9d9d9"];
